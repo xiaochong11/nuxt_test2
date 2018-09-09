c1473dd254af1a66ce3bff60be746c85b34783f7
xiaochong <xiaochong@tempdeMac-mini.local>
2018-09-09 15:38:45 +0800

    添加https证书

--- server/index.js
+++ server/index.js
@@ -162,17 +162,17 @@ io.on('connection', function (socket) {
 })
 //https相关
 if(process.env.NODE_ENV === 'production'){
-    // const privateKey = fs.readFileSync('/etc/letsencrypt/live/yourdomain.com/privkey.pem', 'utf8');
-    // const certificate = fs.readFileSync('/etc/letsencrypt/live/yourdomain.com/cert.pem', 'utf8');
-    // const ca = fs.readFileSync('/etc/letsencrypt/live/yourdomain.com/chain.pem', 'utf8');
-    //
-    // const credentials = {
-    //     key: privateKey,
-    //     cert: certificate,
-    //     ca: ca
-    // };
-    // const httpsServer = https.createServer(credentials, app);
-    // httpsServer.listen(443,host)
+    const privateKey = fs.readFileSync('cert/privkey.pem', 'utf8');
+    const certificate = fs.readFileSync('cert/cert.pem', 'utf8');
+    const ca = fs.readFileSync('cert/chain.pem', 'utf8');
+
+    const credentials = {
+        key: privateKey,
+        cert: certificate,
+        ca: ca
+    };
+    const httpsServer = https.createServer(credentials, app);
+    httpsServer.listen(443,host)
 }else{
 
 }
